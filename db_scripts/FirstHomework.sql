f3fdcd53ba917e55fc58e4c95cef518bcac59c66
VinokurovValeriy <vinokurovvaleriy98@gmail.com>
2020-04-02 05:32:25 +0300

    Some more rows

--- db_scripts/FirstHomework.sql
+++ db_scripts/FirstHomework.sql
@@ -57,12 +57,25 @@ insert into nc_attributes values(null, 4, 'Due Date', 4),
 						(null, 1, 'Access Type', 6),
 						(null, 1, 'Download Speed', 0),
 						(null, 3, 'Service Type', 6),
-						(null, 2, 'Suspend Reason', 0);
+						(null, 2, 'Suspend Reason', 0),
+						(26, 6, 'Activated Period', 2),
+						(27, 7, 'Activated Period', 2),
+						(28, 8, 'Activated Period', 2),
+						(29, 6, 'Product Price', 2),
+						(30, 7, 'Product Price', 2),
+						(31, 8, 'Product Price', 2),
+						(32, 6, 'Order status', 2),
+						(33, 7, 'Order status', 2),
+						(34, 8, 'Order status', 2);
 
 insert into  nc_list_values values(null, 'XDSL'),
 								(null, 'GPON'),
 								(null, 'Postpaid'),
-								(null, 'Prepaid');
+								(null, 'Prepaid'),
+								(29, 'Entering'),
+								(30, 'Processing'),
+								(31, 'Completed'),
+								(32, 'Cancelled');
 
 insert into nc_params values(1, 3, 2, null),
 							(1, 4, null, '100 Mbps'),
@@ -70,7 +83,17 @@ insert into nc_params values(1, 3, 2, null),
 							(3, 6, null, 'Tariff Change'),
 							(4, 3, 1, null),
 							(4, 4, null, '500 Mbps'),
-							(5, 5, 3, null);
+							(5, 5, 3, null),
+                            (1,26,null,5),
+                            (1,29,null,35),
+                            (2,27,null,5),
+                            (2,30,null,35),
+                            (3,27,null,4),
+                            (3,30,null,44),
+                            (4,26,null,3),
+                            (4,29,null,57),
+                            (5,28,null,3),
+                            (5,31,null,60);
 				
 insert into nc_references values(5, 2, 6);
                                 
